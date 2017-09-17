b2f376cb63228bf932c9f7346e1efe2dcdaea8a8
liweihan <weihanli@outlook.com>
2017-09-17 15:28:46 +0800

    submit test and update ci config for temp disable auto test

---
+++ WeihanLi.Common.Test/HelpersTest/ConfigurationHelperTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WeihanLi.Common.Test.HelpersTest
+{
+    public class ConfigurationHelperTest
+    {
+        [Fact]
+        public void AppSettingsTest()
+        {
+            var val = Helpers.ConfigurationHelper.AppSetting("key1");
+            Assert.NotEmpty(val);
+        }
+
+        [Fact]
+        public void AppSettingsModelTest()
+        {
+            var val = Helpers.ConfigurationHelper.AppSetting<SimplePersonModel>("key2");
+            Assert.NotNull(val);
+        }
+    }
+
+    public class SimplePersonModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WeihanLi.Common.Test/HelpersTest/LogHelperTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WeihanLi.Common.Helpers;
+
+namespace WeihanLi.Common.Test.HelpersTest
+{
+    public class LogHelperTest
+    {
+        private static LogHelper logger = new LogHelper(typeof(LogHelperTest));
+
+        static LogHelperTest()
+        {
+            LogHelper.LogInit();
+        }
+    }
+}
